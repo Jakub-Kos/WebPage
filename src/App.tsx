0280f7a82a53f92fbef0d19147d6eb210591d045
Kos Jakub <kubik.kos@gmail.com>
2025-10-12 19:48:56 +0200

    Fix Panel typing (include children) for GitHub Pages build 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useMemo, useRef, useState, PropsWithChildren } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import type { PropsWithChildren } from "react";
 import { create, all } from "mathjs";
 
 const math = create(all, {});
